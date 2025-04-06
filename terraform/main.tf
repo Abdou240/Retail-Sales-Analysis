f6e6e4646e7534bfa3dc5f8b8bf5a4048a0aec2b
Abderrahmen Mansour <abderahmenmansour@yahoo.fr>
2025-04-06 16:09:00 +0200

    adjust gitignor File

---
+++ terraform/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.28.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "vigilant-cider-376613"
+  region  = "europe-west4"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "terraform-demo-bucket-cider-376613"
+  location      = "EU"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
